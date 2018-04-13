3ba91eeafd0558518514f277778ddc4274236a82
Mırt <Mrt@192.168.1.27>
2018-04-13 23:06:15 +0300

    Client Campaing test java dosyaları yüklendi

---
+++ come342homew2/src/Client.java
@@ -0,0 +1,22 @@
+//Author: Zehranur Ayyýldýz	
+//Date: 12/04/2018
+
+public class Client extends Campaign {
+private String companyAddress;
+private String contactName;
+public String companyInformation; 
+ 
+ 
+ 
+public String getClientCompany(){ 
+	String companyInformation="Campaign Name :"+getTitle()+", Campaign Start Date :" +getCampaignStartDate() + 	", Campaign Finish Date : " +getCampaignFinishDate()+", Estimated Cost : "+ getEstimatedCost() ;
+    return companyInformation;
+}
+ 
+ 
+public void addNewCampaigns() {
+
+ 
+ 
+ }
+}
